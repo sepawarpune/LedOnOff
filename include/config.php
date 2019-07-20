55ffe4ab2b9a03d6fc7e0e446b76577d02709c5d
sepawarpune <39512434+sepawarpune@users.noreply.github.com>
2019-07-20 16:58:02 +0530

    Create config.php

---
+++ include/config.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Used to store website configuration information.
+ *
+ * @var string or null
+ */
+function config($key = '')
+{
+    $config = [
+        'name' => 'Arthtech LED Control',
+        'site_url' => '',
+        'pretty_uri' => true,
+        'nav_menu' => [
+            '' => 'Home',
+            'about-us' => 'About Us',
+            'products' => 'Products',
+            'contact' => 'Contact',
+        ],
+        'template_path' => 'template',
+        'content_path' => 'content',
+        'version' => 'v1.0',
+    ];
+    return isset($config[$key]) ? $config[$key] : null;
+}
